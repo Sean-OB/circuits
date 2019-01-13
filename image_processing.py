5032e9badfd5f9988a542b4b44084deb16a50e02
Sean O'Brien <sean.obrien@berkeley.edu>
2019-01-12 22:26:00 -0800

    Added line detection.

--- image_processing.py
+++ image_processing.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import imageio as img
 import numpy as np
 import cv2 as cv
+import skimage
 from skimage import data, color
 from skimage.transform import hough_circle, hough_circle_peaks, probabilistic_hough_line, hough_line, hough_line_peaks
 from skimage.feature import canny
-from skimage.draw import circle_perimeter, line
 from skimage.util import img_as_ubyte
 from skimage.filters import median
 
@@ -14,7 +14,16 @@ def find_all(file_name, show_shapes=False):
     data structure: TODO """
     find_shape(file_name, 'circle', show_shapes=show_shapes)
 
+### MISCELLANEOUS HELPER FUNCTIONS ###
 
+def bw_image(file_name):
+    """
+    Returns a black and white, contrastified, eroded, smoothed image.
+
+    file_name: string file name of an image file to be read."""
+    processed = skimage.morphology.erosion(median(img.imread(file_name, as_gray=True).astype('uint8'), selem=np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])))
+    contrastified = contrastify(processed)
+    return contrastified
 
 def contrastify(arr, midpoint=None):
     """
@@ -24,16 +33,9 @@ def contrastify(arr, midpoint=None):
     midpoint: lower values are mapped to zero, equal or higher values are mapped to max_val
     """
     if not midpoint:
-        midpoint = (np.min(arr) + np.max(arr)) / 2
-    return (arr > midpoint) * 255
-
-def bw_image(file_name):
-    """
-    Returns a black and white, contrastified, smoothed image.
-
-    file_name: string file name of an image file to be read."""
-    image = img.imread(file_name, as_gray=True)
-    return contrastify(img.imread(file_name, as_gray=True)).astype('uint8')
+        midpoint = (np.min(arr) + np.max(arr)) * 0.5
+    arr = (arr > midpoint) * 255
+    return arr
 
 def show_bw(image):
     plt.imshow(image, cmap=plt.cm.gray, interpolation='gaussian')
@@ -52,6 +54,8 @@ def show_plot(file_name):
     image = bw_image(file_name)
     show(contrastify(edgify(image)))
 
+### SHAPE FINDING FUNCTIONS ###
+
 def find_shape(file_name, shape, show_shapes=False):
     """ Finds all shapeures in an image. Types of structurs:
     circles: 'circle'
@@ -66,22 +70,65 @@ def find_shape(file_name, shape, show_shapes=False):
     elif shape == 'line':
         shapes = line_hough(image)
         disp = disp_lines
+    elif shape == 'corner':
+        shapes = corner_detection(image)
+        disp = disp_corners
     if show_shapes:
         disp(shapes, color_image)
     return shapes
-        
 
 ### LINE SECTION ###
 
 def find_lines(file_name, show_lines=False):
-    """Performs the full process of identifying lines ina n image.
+    """Performs the full process of identifying lines in an image."""
+    return find_shape(file_name, 'line', show_shapes=show_lines)
+
+def line_hough(image):
+        
+    # Find edges
+    """
+    image_bytes = median(img_as_ubyte(image))
+    edges = canny(image, sigma=3, low_threshold=10, high_threshold=50)
+    """
+    edges = canny(image, sigma=0.5, low_threshold=0.2, high_threshold=0.3, use_quantiles=True)
+    line_coords = probabilistic_hough_line(edges, threshold=5, line_length=50, line_gap=15, theta=np.pi * np.arange(-1/2, 1/2, 0.01))
+    lines = [Line(c0[0], c0[1], c1[0], c1[1]) for c0, c1 in line_coords]
+    return lines
+
+def disp_lines(lines, color_image):
+    for line in lines:
+        color_image[skimage.draw.line(line.y0, line.x0, line.y1, line.x1)] = (220, 20, 20)
+    print(len(lines))
+    show(color_image)
+
+def consolidate_lines(lines, circles):
+    """ Takes all of the shorter lines returned by the line_hough function and combines them into longer lines.
+
+    Input: 
+        lines: a list of line objects
+        circles: a list of circle objects
+    Output: a list of lines of the same format
+    
+    Strategy: take the first line and find the next closest line.
+              If this line is closer than some threshold, then join the two
+                
+              If the lines are of the same angle type, then combine them into one longer wire
+                Then remove the old wires from the list and call the function on the list including the new wire
+    """
+
+### CORNER SECTION ###
+
+def find_corners(file_name, show_corners=False):
+    """Performs the full process of identifying lines in an image.
     file_name is a string pointing to the location of a parseable image
 
     Returns a list of Lines.
     """
-    return find_shape(file_name, 'line', show_shapes=show_lines)
+    return find_shape(file_name, 'corner', show_shapes=show_corners)
 
-def line_hough(image):
+
+
+def corner_detection(image):
     """ Uses scikit-image's probabilistic Hough line transform to identify lines in an image.
 
     image: numpy image
@@ -104,7 +151,7 @@ def filter_lines(hspace, angles, distances):
         return np.sqrt((line[0] - line[2]) ** 2 + (line[1] - line[3]) ** 2)
 
 
-def disp_lines(lines, color_image):
+def disp_corners(lines, color_image):
     """ Displays the detected line segments """
     color_image[lines > 0.15 * np.max(lines)] = (220, 20, 20)
     show(color_image)
@@ -135,7 +182,6 @@ def circle_hough(image, max_circles=15):
     Returns a list of filtered Circles
     """
     # Find edges
-    image_bytes = median(img_as_ubyte(image))
     edges = canny(image, sigma=3, low_threshold=10, high_threshold=50)
 
     # Detect radii through Hough transform
@@ -147,7 +193,7 @@ def circle_hough(image, max_circles=15):
 
 def disp_circles(circles, color_image):
     for circ in circles:
-        cy, cx = circle_perimeter(circ.y, circ.x, circ.radius)
+        cy, cx = skimage.draw.circle_perimeter(circ.y, circ.x, circ.radius)
         color_image[cy, cx] = (220, 20, 20)
     show(color_image)
 
@@ -195,14 +241,69 @@ class Line:
         self.y0 = y0
         self.x1 = x1
         self.y1 = y1
+        if self.x0 - self.x1 == 0:
+            self.angle = 90
+            self.type = 'vert'
+        else:
+            angle = np.arctan((self.y0 - self.y1) / (self.x0 - self.x1)) * 180 / np.pi
+            if -90 <= angle < -75 or angle >= 52.5:
+                self.angle = 90
+                self.type = 'vert'
+            elif -75 <= angle < -52.5:
+                self.angle = -60
+                self.type = 'neg-diag'
+            elif -52.5 <= angle < -37.5:
+                self.angle = -45
+                self.type = 'neg-diag'
+            elif -37.5 <= angle < -15:
+                self.angle = -30
+                self.type = 'neg-diag'
+            elif -15 <= angle < 15:
+                self.angle = 0
+                self.type = 'horz'
+            elif 15 <= angle < 37.5:
+                self.angle = 30
+                self.type = 'pos-diag'
+            elif 37.5 <= angle < 52.5:
+                self.angle = 45
+                self.type = 'pos-diag'
+            elif 52.5 <= angle < 75:
+                self.angle = 60
+                self.type = 'pos-diag'
 
     def from_polar(d0, a0, d1, a1, starting=(0, 0)):
         """ Angles are in radians """
         x0 = starting[0] + d0 * np.cos(a0)
-        y0 = starting[1] + d0 * np.cos(a0)
+        y0 = starting[1] + d0 * np.sin(a0)
         x1 = starting[0] + d1 * np.cos(a1)
-        y1 = starting[1] + d1 * np.cos(a1)
+        y1 = starting[1] + d1 * np.sin(a1)
         return Line(x0, y0, x1, y1)
+
+    def length(self):
+        """ Returns the Euclidean length of the line """
+        dx, dy = self.x1 - self.x0, self.y1 - self.y0
+        return np.sqrt(dx ** 2 + dy ** 2)
+
+    def coords(self):
+        """ Returns the coordinates of endpoints in the format ((x0, y0), (x1, y1)) """
+        return ((self.x0, self.y0), self.x1, self.y1)
+
+    def endpoint_min_dist(self, line):
+        """ Returns the minimum distance between the endpoints of two lines """
+        dists = []
+        for i in range(2):
+            for j in range(2):
+                dists += coord_dist(self.coords[i], line.coords[j])
+        return min(dists)
+
+    def coord_dist(c0, c1):
+        """ c0 and c1 are coordinate tuples of the form (x, y) """
+        return np.sqrt((c0[0] - c1[0]) ** 2 + (c0[1] - c1[1]) ** 2)
+
+    def line_min_dist(self, line):
+        """ Returns the minimum Euclidean distance from another line to this one.
+        Note that the minimum distance is the orthogonal projection of the difference vector onto this line."""
+        
 
 
 simple = './ex_circuits/easy_circuit.jpg'
@@ -211,4 +312,5 @@ three_circles = './ex_circuits/three_circles.jpg'
 
 # UNCOMMENT THIS LINE TO TEST (you can replace the file name with the others if you prefer)
 # find_all(simple, show_shapes=True)
-find_lines(simple, show_lines=True)
+find_lines(three_circles, show_lines=True)
+#image = bw_image(simple)
